52fc67fb9da6d9529ed07526770eee705e3043fa
Thomas Butler <trbiv@proton.me>
2024-02-25 22:05:06 -0700

    use slot urls on pred env

--- react-threejs/dojoConfig.ts
+++ react-threejs/dojoConfig.ts
@@ -1,6 +1,12 @@
 import manifest from "../contracts/target/dev/manifest.json";
 import { createDojoConfig } from "@dojoengine/core";
 
+const toriiUrl = import.meta.env.VITE_TORII_URL;
+const katanaUrl = import.meta.env.VITE_KATANA_URL;
+const env = import.meta.env.VITE_ENV;
+
 export const dojoConfig = createDojoConfig({
   manifest,
+  toriiUrl: env === "production" ? toriiUrl : "http://localhost:8080",
+  rpcUrl: env === "production" ? katanaUrl : "http://localhost:5050",
 });
