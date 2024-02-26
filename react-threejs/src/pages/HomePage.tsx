86158d6da2caacce7f9a4248cea9f90df171b4cb
Thomas Butler <trbiv@proton.me>
2024-02-25 19:35:40 -0700

    logo from src dir

--- react-threejs/src/pages/HomePage.tsx
+++ react-threejs/src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
       <div className="flex justify-center items-center flex-col mt-20">
         <h1 className="text-6xl">Welcome to Stark Sprouts</h1>
         <img
-          src="../../assets/logo.png"
+          src="../assets/logo.png"
           alt="Stark Sprouts"
           width={200}
           height={200}
