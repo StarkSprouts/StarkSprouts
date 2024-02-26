23b49baef29a203529573fb542ac291b1143fec1
Thomas Butler <trbiv@proton.me>
2024-02-26 01:00:58 -0700

    garden plot with borders

--- react-threejs/src/gameComponents/AssetLoader.tsx
+++ react-threejs/src/gameComponents/AssetLoader.tsx
@@ -9,26 +9,48 @@ type AssetLoaderProps = {
   children: React.ReactNode;
 };
 
-export type AssetsType = {
-  grass: any;
-  plot: any;
+const texturePaths = {
+  grass: "src/textures/tiles/grass/grass_15.png",
+  plot: "src/textures/tiles/sand/sand_07.png",
+  topLeftCorner: "src/textures/tiles/sand/sand_00.png",
+  topRightCorner: "src/textures/tiles/sand/sand_02.png",
+  bottomRightCorner: "src/textures/tiles/sand/sand_14.png",
+  bottomLeftCorner: "src/textures/tiles/sand/sand_12.png",
+  leftEdge: "src/textures/tiles/sand/sand_06.png",
+  rightEdge: "src/textures/tiles/sand/sand_08.png",
+  topEdge: "src/textures/tiles/sand/sand_01.png",
+  bottomEdge: "src/textures/tiles/sand/sand_13.png",
 };
 
 export const AssetLoader = ({ children }: AssetLoaderProps) => {
   const textures = useLoader(TextureLoader, [
-    "src/textures/tiles/Grass/grass_15.png",
-    "src/textures/tiles/Sand/sand_09.png",
+    texturePaths.grass,
+    texturePaths.plot,
+    texturePaths.topLeftCorner,
+    texturePaths.topRightCorner,
+    texturePaths.bottomRightCorner,
+    texturePaths.bottomLeftCorner,
+    texturePaths.leftEdge,
+    texturePaths.rightEdge,
+    texturePaths.topEdge,
+    texturePaths.bottomEdge,
   ]);
 
   const assets = {
     grass: textures[0],
     plot: textures[1],
+    topLeftCorner: textures[2],
+    topRightCorner: textures[3],
+    bottomRightCorner: textures[4],
+    bottomLeftCorner: textures[5],
+    leftEdge: textures[6],
+    rightEdge: textures[7],
+    topEdge: textures[8],
+    bottomEdge: textures[9],
   };
 
   return (
-    <AssetsContext.Provider value={assets as AssetsType}>
-      {children}
-    </AssetsContext.Provider>
+    <AssetsContext.Provider value={assets}>{children}</AssetsContext.Provider>
   );
 };
 
--- react-threejs/src/gameComponents/Tile.tsx
+++ react-threejs/src/gameComponents/Tile.tsx
@@ -1,33 +1,87 @@
 import React from "react";
 import { useThree } from "@react-three/fiber";
 import { useAssets } from "@/gameComponents/AssetLoader";
-import type { AssetsType } from "@/gameComponents/AssetLoader";
 import { PlaneGeometry, MeshBasicMaterial, Mesh } from "three";
 
 type TileProps = {
-  type: string;
+  type: TileType;
   position: [number, number];
 };
 
-type TileType = "G" | "P";
+export type AssetsType = {
+  grass: any;
+  plot: any;
+  topLeftCorner: any;
+  topRightCorner: any;
+  bottomRightCorner: any;
+  bottomLeftCorner: any;
+  leftEdge: any;
+  rightEdge: any;
+  topEdge: any;
+  bottomEdge: any;
+};
+
+export enum TileType {
+  Grass = "G",
+  Plot = "P",
+  TopLeftCorner = "1",
+  TopRightCorner = "2",
+  BottomLeftCorner = "3",
+  BottomRightCorner = "4",
+  LeftEdge = "L",
+  RightEdge = "R",
+  TopEdge = "T",
+  BottomEdge = "B",
+}
 
 export const Tile = ({ type, position }: TileProps) => {
   const { scene } = useThree();
-  const { grass, plot } = useAssets() as AssetsType;
+  const {
+    grass,
+    plot,
+    topLeftCorner,
+    topRightCorner,
+    bottomRightCorner,
+    bottomLeftCorner,
+    leftEdge,
+    rightEdge,
+    topEdge,
+    bottomEdge,
+  } = useAssets() as AssetsType;
 
   // Example: Add different objects based on the tile type
   let texture;
-  switch (type) {
-    case "G":
-      // Add grass tile
+  switch (type as TileType) {
+    case TileType.Grass:
+      console.log("Grass");
       texture = grass;
       break;
-    case "P":
-      // Add soil plot
+    case TileType.Plot:
       texture = plot;
       break;
-    default:
-      // Default tile if needed
+    case TileType.TopLeftCorner:
+      texture = topLeftCorner;
+      break;
+    case TileType.TopRightCorner:
+      texture = topRightCorner;
+      break;
+    case TileType.BottomRightCorner:
+      texture = bottomRightCorner;
+      break;
+    case TileType.BottomLeftCorner:
+      texture = bottomLeftCorner;
+      break;
+    case TileType.LeftEdge:
+      texture = leftEdge;
+      break;
+    case TileType.RightEdge:
+      texture = rightEdge;
+      break;
+    case TileType.TopEdge:
+      texture = topEdge;
+      break;
+    case TileType.BottomEdge:
+      texture = bottomEdge;
       break;
   }
 
