9c36cbf44d9f675ac513181ffc4c8e692586f790
Thomas Butler <trbiv@proton.me>
2024-02-25 19:30:20 -0700

    updated home page and nav bar

--- react-threejs/src/components/ui/nav.tsx
+++ react-threejs/src/components/ui/nav.tsx
@@ -2,8 +2,15 @@ import { Button } from "./button";
 
 export const Nav = () => {
   return (
-    <nav className="bg-gray">
-      <Button />
+    <nav className="bg-gray flex justify-end">
+      <div>
+        <Button>
+          <a href="/game">Launch App</a>
+        </Button>
+        <Button>
+          <a href="/test">Test App</a>
+        </Button>
+      </div>
     </nav>
   );
 };
--- react-threejs/src/pages/HomePage.tsx
+++ react-threejs/src/pages/HomePage.tsx
@@ -4,12 +4,15 @@ export default function HomePage() {
   return (
     <main className="relative w-screen h-screen">
       <Nav />
-      <img
-        src="../../assets/logo.png"
-        alt="Stark Sprouts"
-        width={100}
-        height={100}
-      />
+      <div className="flex justify-center items-center flex-col mt-20">
+        <h1 className="text-6xl">Welcome to Stark Sprouts</h1>
+        <img
+          src="../../assets/logo.png"
+          alt="Stark Sprouts"
+          width={200}
+          height={200}
+        />
+      </div>
     </main>
   );
 }
