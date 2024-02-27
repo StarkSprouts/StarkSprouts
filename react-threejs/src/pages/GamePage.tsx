82d96d1e2bdf714eb7104dd7a4d8f8a4d9523531
Thomas Butler <trbiv@proton.me>
2024-02-26 19:47:42 -0700

    dont show garden until initialized

--- react-threejs/src/pages/GamePage.tsx
+++ react-threejs/src/pages/GamePage.tsx
@@ -4,9 +4,11 @@ import useWindowSize from "@/hooks/useWindowSize";
 import { GardenCells } from "@/gameComponents/GardenCells";
 import { useDojo } from "@/dojo/useDojo";
 import { StyledButton } from "@/components/StyledButton";
+import { useState } from "react";
 
 export default function GamePage() {
   const [width, height] = useWindowSize();
+  const [gardenInitialized, setGardenInitialized] = useState(false);
   const {
     account: { account },
     setup: {
@@ -15,16 +17,22 @@ export default function GamePage() {
     },
   } = useDojo();
 
-  const handleInitGarden = () => {
+  const handleInitGarden = async () => {
     console.log("init world");
-    initializeGarden(account);
+    await initializeGarden(account);
+    setGardenInitialized(true);
   };
 
-  return (
-    <div className="flex w-screen h-screen justify-center items-center bg-slate-900">
+  if (!gardenInitialized) {
+    return (
       <div className="absolute z-10 top-5 left-2 flex flex-col space-y-2">
         <StyledButton label="Init Garden" onPress={handleInitGarden} />
       </div>
+    );
+  }
+
+  return (
+    <div className="flex w-screen h-screen justify-center items-center bg-slate-900">
       <Game canvasWidth={width} canvasHeight={height}>
         <WorldScene />
         <GardenCells />
