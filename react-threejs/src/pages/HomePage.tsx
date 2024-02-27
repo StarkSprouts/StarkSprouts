63de164cbfba2c9ee9faa6019fa63fef0c9e2b7a
Matthew Coburn <mattcoburn24@gmail.com>
2024-02-26 17:30:08 -0700

    home page updates

--- react-threejs/src/pages/HomePage.tsx
+++ react-threejs/src/pages/HomePage.tsx
@@ -1,19 +1,18 @@
 import { TopBar } from "@/components/TopBar";
+import { StyledButton } from "../components/StyledButton";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-row w-screen h-screen bg-slate-400">
-      <div>
-        <TopBar />
-      </div>
-      <div className="flex justify-center items-center flex-col">
-        <h1 className="text-6xl">Welcome to Stark Sprouts</h1>
-        <img
-          src="../static/logo.png"
-          alt="Stark Sprouts"
-          width={200}
-          height={200}
-        />
+    <div className="flex flex-col h-screen bg-banner bg-center bg-cover bg-no-repeat">
+      <TopBar />
+      <div className="flex-grow flex justify-center mt-5">
+        <div className="flex items-center flex-col mb-4 ml-8">
+          <h1 className="text-6xl mb-4 text-white">Welcome to Stark Sprouts</h1>
+
+          <StyledButton>
+            <a href="/game">Launch App</a>
+          </StyledButton>
+        </div>
       </div>
     </div>
   );
